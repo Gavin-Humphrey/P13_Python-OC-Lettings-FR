3b48f3c7c945527b3a96c6afc9308b514fcafb33
GmGith <87491753+GmGith@users.noreply.github.com>
2023-03-07 18:24:19 +0100

    Réduction de diverses dettes techniques/refonte de l'architecture modulaire

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -19,7 +19,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'oc_lettings_site.apps.OCLettingsSiteConfig',
+    'homepage.apps.HomepageConfig',
+    'lettings.apps.LettingsConfig',
+    'profiles.apps.ProfilesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
